eae50964fd3e146402e6f94f7c2a13302174b115
PengXJ <xingjie.peng@topplusvision.com>
2020-05-06 16:30:43 +0800

    测试类
    测试类
    
    Co-Authored-By: pengxj-plus <pengxj-plus@users.noreply.github.com>

---
+++ src/test/java/com/topplus/search/OrderProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.topplus.search;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
